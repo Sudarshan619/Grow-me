e10c3fa5e7cec424c2b8befc8799947493b152e0
sudu <pujarsudarshan@gmail.com>
2024-07-03 20:46:23 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import UserForm from './components/userform'
 import { Routes, Route, Navigate } from 'react-router-dom';
